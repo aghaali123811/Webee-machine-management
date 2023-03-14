03978d2cb44e4f707c24cf423757544d429b7353
aghaali123811 <aghaali123811@gmail.com>
2023-03-14 11:02:19 +0500

    improvements

--- src/store/Actions/authAction.tsx
+++ src/store/Actions/authAction.tsx
@@ -2,13 +2,7 @@
 import {store} from '../Store';
 
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const SET_ERROR = 'SET_ERROR';
-export const SET_LOADING = 'SET_LOADING';
-export const SET_COLLECTION_POINT = 'SET_COLLECTION_POINT';
-export const SET_DELIVERY_ADDRESS = 'SET_DELIVERY_ADDRESS';
-export const SET_CLEAR_DATA = 'SET_CLEAR_DATA';
-export const SET_ORDERS = 'SET_ORDERS';
-export const SET_UPDATED_DATA = 'SET_UPDATED_DATA';
+
 
 
 
